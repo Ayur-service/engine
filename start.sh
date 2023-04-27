b49c101f8806cbe320d46664c811297fd38975ea
cosmicoppai <kanakchaudhari12@gmail.com>
2023-04-27 17:54:30 +0530

    add script to clone repos and start containers

--- start.sh
+++ start.sh
@@ -1,1 +1,6 @@
+#!/usr/bin/env bash
 
+repos={"https://github.com/Ayur-service/Checkups.git", "https://github.com/Ayur-service/auth.git",
+"https://github.com/Ayur-service/Hospital.git", "https://github.com/Ayur-service/User.git"}
+
+for f in $repos; do git clone "$f"; done  && docker-compose up -d
