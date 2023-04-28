ed22e580ae6de17a47f43552d113b81b0ec15d8a
cosmicoppai <kanakchaudhari12@gmail.com>
2023-04-28 06:02:29 +0530

    fixed error

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-repos={"https://github.com/Ayur-service/Checkups.git", "https://github.com/Ayur-service/auth.git",
-"https://github.com/Ayur-service/Hospital.git", "https://github.com/Ayur-service/User.git"}
+repos=("https://github.com/Ayur-service/Checkups.git" "https://github.com/Ayur-service/auth.git"
+"https://github.com/Ayur-service/Hospital.git" "https://github.com/Ayur-service/User.git")
 
-for f in $repos; do git clone "$f"; done  && docker-compose up -d
+for f in "${repos[@]}"; do git clone "$f"; done && docker-compose up -d
