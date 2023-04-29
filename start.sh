c40ca2ee0ff035413ebb02e3523b26ba92dfa398
cosmicoppai <kanakchaudhari12@gmail.com>
2023-04-29 17:33:03 +0530

    update start smd

--- start.sh
+++ start.sh
@@ -1,6 +1,3 @@
 #!/usr/bin/env bash
 
-repos=("https://github.com/Ayur-service/Checkups.git" "https://github.com/Ayur-service/auth.git"
-"https://github.com/Ayur-service/Hospital.git" "https://github.com/Ayur-service/User.git")
-
-for f in "${repos[@]}"; do git clone "$f"; done && docker-compose up -d
+docker-compose up -d
